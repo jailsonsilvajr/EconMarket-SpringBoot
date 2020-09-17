5211b42536c05661d3e77307c979af6721f30676
Jailson <jailson@jailsonG40>
2020-09-17 12:16:21 -0300

    initial commit

---
+++ src/main/java/br/com/springboot/econmarket/EconMarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.econmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springboot/econmarket/EconMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.econmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
